2567ebae20c2206dcd6bf898c8380c67301c83ae
metinfarukbiyik <mb.metinbiyik@gmail.com>
2025-07-30 00:29:40 +0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Navigation, BlogPost, Footer } from './components';
 import { HomePage, EquipmentPage, AboutPage, NotFoundPage, BlogPage } from './pages';
@@ -6,25 +6,34 @@ import './styles/reset.css';
 import './styles/variables.css';
 import './styles/App.css';
 
+function AppContent() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="app">
+      <Navigation />
+      
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ekipmanlar" element={<EquipmentPage />} />
+          <Route path="/hakkimda" element={<AboutPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      {!isHomePage && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <HelmetProvider>
       <Router>
-        <div className="app">
-          <Navigation />
-          
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/ekipmanlar" element={<EquipmentPage />} />
-              <Route path="/hakkimda" element={<AboutPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/blog/:slug" element={<BlogPost />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <AppContent />
       </Router>
     </HelmetProvider>
   );
